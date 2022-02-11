91ebac3ede4d5e9bcd4e45bdd858a8b52ab3fa91
TyZen <77071313+IIamTyZen@users.noreply.github.com>
2022-02-11 18:45:24 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,21 @@ AOS.init({
     duration: 1200,
   })
   
+  function toggle() {
+    var el = document.getElementById("theme_change");
+    if (el.href.match("assets/css/dark_styles.css")) {
+        el.href = "assets/css/white_styles.css";    
+    }
+    else {
+        el.href = "assets/css/dark_styles.css";  
+    }
+
+    var al = document.getElementById("change_img");
+    if (al.src.match("/assets/img/sun.png")) {
+        al.src = "/assets/img/moon.png";    
+    }
+    else {
+        al.src = "/assets/img/sun.png";  
+    }
+}
+ 
